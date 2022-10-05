16e53fe881f645ddfa825c3dc6816a42732cb4e8
Juantre82 <Juantre82@gmail.com>
2022-10-05 04:46:43 -0500

    code error

--- public/index.js
+++ public/index.js
@@ -15,7 +15,7 @@ function Spa() {
               }}
               >
        <div className="container" style={{padding: "20px"}}>
-        <Route path="/" exact component={Home} />
+        <Route path="/home.js" exact component={Home} />
         <Route path="/CreateAccount" component={CreateAccount} />        
         <Route path="/login" component={Login} />
         <Route path="/deposit" component={Deposit} />
