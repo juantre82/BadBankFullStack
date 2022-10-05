c637090639bf3940a8c302310b097cd0b93cf5a5
Juantre82 <Juantre82@gmail.com>
2022-10-05 00:59:37 -0500

    changes

--- public/index.js
+++ public/index.js
@@ -1,26 +1,35 @@
 function Spa() {
-    return (
-      <HashRouter>
-        <div>
-          <NavBar/>        
-          <UserContext.Provider value={{user:{}}}>
-            <div className="container" style={{padding: "20px"}}>
-              <Route path="/" exact component={Home} />
-              <Route path="/CreateAccount/" component={CreateAccount} />
-              <Route path="/login/" component={Login} />
-              <Route path="/deposit/" component={Deposit} />
-              <Route path="/withdraw/" component={Withdraw} />
-              {/* <Route path="/transactions/" component={Transactions} /> */}
-              <Route path="/balance/" component={Balance} />
-              {/*<Route path="/alldata/" component={AllData} /> */}
-            </div>
-          </UserContext.Provider>
+  return (
+    <HashRouter>
+      <div>
+      <NavBar />
+      <UserContext.Provider value={{
+          users:[
+            {
+              name:'',
+              email:'',
+              password:'',
+              balance: 0
+              }
+              ]
+              }}
+              >
+       <div className="container" style={{padding: "20px"}}>
+        <Route path="/" exact component={Home} />
+        <Route path="/CreateAccount" component={CreateAccount} />        
+        <Route path="/login" component={Login} />
+        <Route path="/deposit" component={Deposit} />
+        <Route path="/withdraw" component={Withdraw} />
+        <Route path="/balance" component={Balance} />
+        <Route path="/alldata" component={AllData} />
         </div>
-      </HashRouter>
-    );
-  }
-  
-  ReactDOM.render(
-    <Spa/>,
-    document.getElementById('root')
+      </UserContext.Provider>
+      </div>
+    </HashRouter>
   );
+}
+
+ReactDOM.render(
+  <Spa />,
+  document.getElementById('root')
+)
